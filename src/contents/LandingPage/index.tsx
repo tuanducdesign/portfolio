6d2b358ed4119cbad37961c88369f33eac15a0a3
ashalfarhan <ashalfarhan8@gmail.com>
2022-04-24 20:27:44 +0700

    fix

--- src/contents/LandingPage/index.tsx
+++ src/contents/LandingPage/index.tsx
@@ -1,3 +1,3 @@
-export * from './FeaturedPeronalProjects'
-export * from './HeroSection'
-export * from './IntroductionSection'
+export * from './FeaturedPeronalProjects';
+export * from './HeroSection';
+export * from './IntroductionSection';
