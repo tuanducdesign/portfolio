c4b278c5a67c7d6cb1c038daece5928ec6a07bbb
ashalfarhan <ashalfarhan8@gmail.com>
2022-04-26 10:53:28 +0700

    gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
 import '../styles/global.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={['light', 'dark']} attribute="class" defaultTheme="dark">
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-0TZP917KRV" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-0TZP917KRV');`}
+      </Script>
       <Component {...pageProps} />
     </ThemeProvider>
   );
