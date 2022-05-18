01898d99ed6440a433271f57a2e4175c722fd809
ashalfarhan <ashalfarhan8@gmail.com>
2022-05-18 11:43:15 +0700

    feat: animated theme switcher

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,9 +1,31 @@
 import Link from 'next/link';
-import { Button } from '../Button';
-import { FiMoon, FiSun } from 'react-icons/fi';
 import { useTheme } from 'next-themes';
-import clsx from 'clsx';
 import { Container } from '../Container';
+import { motion } from 'framer-motion';
+
+const hideIcon = {
+  pathLength: 0,
+  opacity: 0,
+};
+
+const showIcon = {
+  pathLength: 1,
+  opacity: 1,
+  transition: {
+    pathLength: { type: 'spring', duration: 1, bounce: 0 },
+    opacity: { duration: 0.8 },
+  },
+};
+
+const moonVariants = {
+  light: hideIcon,
+  dark: showIcon,
+};
+
+const sunVariants = {
+  light: showIcon,
+  dark: hideIcon,
+};
 
 export const Header = () => {
   const { theme, setTheme } = useTheme();
@@ -14,7 +36,7 @@ export const Header = () => {
 
   return (
     <header className="flex sticky top-0 z-50 backdrop-blur-sm border-b border-b-slate-700">
-      <Container className="flex justify-between py-2 px-2">
+      <Container className="flex justify-between items-center py-2 px-2">
         <Link href="/" passHref>
           <a
             className="font-bold text-3xl italic text-black-primary dark:text-white-text inline-flex items-center"
@@ -34,20 +56,67 @@ export const Header = () => {
             </svg>
           </a>
         </Link>
-        <Button
+        <button
           title="Switch Theme"
-          className="rounded-lg px-4 max-w-[32px] overflow-hidden"
+          className="rounded-lg flex items-center justify-center p-2"
           onClick={toggleDarkMode}
         >
-          <div
-            className={clsx('flex gap-x-2 w-16 transition-all', [
-              isDark ? '-translate-x-2' : '-translate-x-9',
-            ])}
+          <motion.svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="stroke-current"
+            initial={false}
+            animate={theme}
           >
-            <FiMoon size={18} />
-            <FiSun size={18} />
-          </div>
-        </Button>
+            {/* Moon */}
+            <motion.path
+              variants={moonVariants}
+              d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"
+              custom={1}
+            ></motion.path>
+            {/* SUN */}
+            <motion.circle variants={sunVariants} cx="12" cy="12" r="5"></motion.circle>
+            <motion.line variants={sunVariants} x1="12" y1="1" x2="12" y2="3"></motion.line>
+            <motion.line variants={sunVariants} x1="12" y1="21" x2="12" y2="23"></motion.line>
+            <motion.line
+              variants={sunVariants}
+              x1="4.22"
+              y1="4.22"
+              x2="5.64"
+              y2="5.64"
+            ></motion.line>
+            <motion.line
+              variants={sunVariants}
+              x1="18.36"
+              y1="18.36"
+              x2="19.78"
+              y2="19.78"
+            ></motion.line>
+            <motion.line variants={sunVariants} x1="1" y1="12" x2="3" y2="12"></motion.line>
+            <motion.line variants={sunVariants} x1="21" y1="12" x2="23" y2="12"></motion.line>
+            <motion.line
+              variants={sunVariants}
+              x1="4.22"
+              y1="19.78"
+              x2="5.64"
+              y2="18.36"
+            ></motion.line>
+            <motion.line
+              variants={sunVariants}
+              x1="18.36"
+              y1="5.64"
+              x2="19.78"
+              y2="4.22"
+            ></motion.line>
+          </motion.svg>
+        </button>
       </Container>
     </header>
   );
