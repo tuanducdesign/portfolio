3eb0f20643f45c6a31d9dcc6fa64efb59e111bc4
ashalfarhan <ashalfarhan8@gmail.com>
2022-05-19 23:12:58 +0700

    tech stackbar

--- src/components/TechStackBar.tsx
+++ src/components/TechStackBar.tsx
@@ -8,13 +8,15 @@ export const TechStackBar = ({
   title?: string;
 }) => {
   return (
-    <div className="flex gap-x-4 p-2 rounded-md mt-1 items-center">
+    <div className="flex p-2 rounded-md mt-1 md:items-center flex-col md:flex-row">
       {title && <span className="font-bold flex-1 text-lg">{title}</span>}
-      {technologies.map((t, idx) => (
-        <span key={t + idx}>
-          <TechIcon name={t} />
-        </span>
-      ))}
+      <div className="flex gap-2 justify-end">
+        {technologies.map((t, idx) => (
+          <span key={t + idx}>
+            <TechIcon name={t} />
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
