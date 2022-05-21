4fb770c6805b20d7d51f6ce6ea1c30088382ee49
ashalfarhan <ashalfarhan8@gmail.com>
2022-05-21 22:17:33 +0700

    fix: adjust hire page

--- src/pages/hire.tsx
+++ src/pages/hire.tsx
@@ -1,16 +1,17 @@
 import { Seo, Layout, Button, Markdown } from '@site/components';
 import { profile } from '@site/config';
-import { getContent } from '@site/utils';
+import { parseMarkdown } from '@site/libs';
+import { getFileContent } from '@site/utils';
 import { InferGetStaticPropsType } from 'next';
 
 export default function HirePage({
-  data,
+  content,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <Layout>
       <Seo title="Hire Me!" />
       <div className="container mx-auto pb-12">
-        <Markdown className="md:mx-auto my-12 p-4" content={data.content} />
+        <Markdown className="md:mx-auto my-12 p-4" content={content} />
         <div className="max-w-xl mx-auto md:space-x-4 flex md:flex-row flex-col md:space-y-0 space-y-4 px-4">
           <Button
             className="flex-auto"
@@ -34,10 +35,11 @@ export default function HirePage({
 }
 
 export const getStaticProps = async () => {
-  const content = await getContent({
-    slug: 'career',
+  const raw = getFileContent({
+    filename: 'hire',
   });
+  const content = await parseMarkdown(raw);
   return {
-    props: { data: content },
+    props: { content },
   };
 };
