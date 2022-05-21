a73d4b603a6d47996c0016a76b317823c1904447
ashalfarhan <ashalfarhan8@gmail.com>
2022-05-21 23:48:40 +0700

    chore: completely remove

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-const siteUrl = 'https://ashal.me';
-
-/** @type {import("next-sitemap").AdditionalPathsConfig} */
-module.exports = {
-  siteUrl,
-  generateRobotsTxt: true,
-  changefreq: 'monthly',
-  priority: 0.9,
-};
