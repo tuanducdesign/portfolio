5d586c0aa6850c374483b43894b990193902a958
ashalfarhan <ashalfarhan8@gmail.com>
2022-06-08 19:17:56 +0700

    fix: smooth scroll

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,12 +2,12 @@ import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" className="scroll-smooth">
       <Head>
         <link rel="preconnect" href="https://ik.imagekit.io" />
         <link rel="preconnect" href="https://www.googletagmanager.com" />
       </Head>
-      <body className="scroll-smooth bg-light-primary text-dark-primary dark:bg-dark-primary dark:text-light-primary transition-colors duration-300">
+      <body className="bg-light-primary text-dark-primary dark:bg-dark-primary dark:text-light-primary transition-colors duration-300">
         <Main />
         <NextScript />
       </body>
